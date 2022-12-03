503ae156c45c16d53a2e03455a0161b6bcb467a5
Zstephen1 <nzoux01@gmail.com>
2022-12-03 15:57:21 +0100

    first commit

---
+++ src/test/java/com/groupeuisi/ParcInfo/ParcInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeuisi.ParcInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
